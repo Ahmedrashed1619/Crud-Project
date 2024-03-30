6dbcf34c23d99819c3052e7cc53888c64624cd63
Ahmedrashed1619 <ar9353062@gmail.com>
2024-03-30 17:04:38 +0200

    add new product

--- src/App.tsx
+++ src/App.tsx
@@ -41,15 +41,13 @@ function App() {
     price: ''
   }
 
+  const [products, setProducts] = useState<IProductProps[]>(productList);
+
   const [product, setProduct] = useState<IProductProps>(defaultProduct);
 
   const [errors, setErrors] = useState(defaultErrors);
 
   const [tempColors, setTempColors] = useState<string[]>([]);
-
-  console.log(tempColors);
-  
-
 
   // --------------------RENDER-----------------
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
@@ -66,7 +64,7 @@ function App() {
     })
   }
 
-  const renderProductsList = productList.map(product => 
+  const renderProductsList = products.map(product => 
     <ProductCard
       key={product.id}
       product={product}
@@ -88,7 +86,13 @@ function App() {
     <Color
       key={color}
       color={color}
-      onClick={() => setTempColors(prev => [...prev, color])}
+      onClick={() => {
+        if(tempColors.includes(color)) {
+          setTempColors(prev => [...prev.filter(item => item !== color)])
+          return
+        }
+        setTempColors(prev => [...prev, color])
+      }}
     />
   )
 
@@ -109,7 +113,10 @@ function App() {
     if(hasErrorMsg){
       console.log('trueeee');
       // send a request api to server
-      return
+      setProducts(prev => [...prev, {...product, colors: tempColors}])
+      setProduct(defaultProduct);
+      setTempColors([])
+      closeModal()
     }
 
     setErrors(errors);
@@ -123,16 +130,30 @@ function App() {
 
 
   return (
-    <main className="container py-7">
+    <main className="container py-12">
+      <div className="flex md:flex-row flex-col items-center justify-between gap-y-8 my-10">
+        <h1 className=" text-5xl font-extrabold">Latest <span className=" text-indigo-700">Products</span></h1>
+        <Button type='button' width="w-fit" className={'bg-indigo-700 hover:bg-indigo-500'} onClick={openModal}>Build now</Button>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-5">
         {renderProductsList}
       </div>
       <MyDialog isOpen={isOpen} closeModal={closeModal} title={'Add a new Product'}>
         <form className="space-y-4" onSubmit={submitHandler}>
           {renderFormInputsList}
+
+          <div className="flex flex-wrap items-center">
+            {tempColors.map(el => (
+              <span key={el} className=" text-white px-2 py-1 me-1 mb-1 font-semibold text-xs rounded-md" style={{backgroundColor: el}}>
+                {el}
+              </span>
+            ))}
+          </div>
+
           <div className="flex flex-wrap items-center mb-2">
             {renderColorsList}
           </div>
+
           <div className="flex items-center justify-between space-x-3">
             <Button type="submit" className={'bg-indigo-700 hover:bg-indigo-500'}>Submit</Button>
             <Button type="reset" className={'bg-gray-400 hover:bg-gray-600'} onClick={onCloseHandler}>cancel</Button>
--- src/components/ui/Color/Color.tsx
+++ src/components/ui/Color/Color.tsx
@@ -2,7 +2,11 @@ import { IColorProps } from "../../../interfaces"
 
 function Color({color, ...rest}: IColorProps) {
   return (
-    <span style={{backgroundColor: color}} className={`w-6 h-6 rounded-full cursor-pointer me-1.5 mb-3`} {...rest} />
+    <span
+      className={`w-6 h-6 rounded-full cursor-pointer me-1.5 mb-3`}
+      style={{backgroundColor: color}}
+      {...rest}
+    />
   )
 }
 
